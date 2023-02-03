f003762a4db04552b85371253826076bcd4cb63d
Portix <92767862+Portix-S@users.noreply.github.com>
2023-02-03 16:06:26 -0300

    Roguelike Update

---
+++ Roguelike Cosmos/Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,24 @@
+using Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerCombat : MonoBehaviour
+{
+    [SerializeField] private PlayerData _playerData;
+
+    [Header("Animation")]
+    public Animator playerAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            playerAnimator.SetBool("isPunching", true);
+            Debug.Log(_playerData.attackDamage);
+        }
+    }
+}
